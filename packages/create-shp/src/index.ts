2c3eeda22c6b46b8f2395ca9238283ef9ae9a097
liuxspro <liuxspro@gmail.com>
2026-01-04 14:35:37 +0800

    Chore: update libs to 0.1.12

--- packages/create-shp/src/index.ts
+++ packages/create-shp/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./bjwj.ts";
 export * from "./shp.ts";
 export * from "./source/index.ts";
+export * from "./utils.ts";
---
+++ packages/create-shp/src/utils.ts
@@ -0,0 +1,2 @@
+export { MultiPolygon, Polygon, Ring } from "@liuxspro/libs/geo";
+export type { Point } from "@liuxspro/libs/geo";
