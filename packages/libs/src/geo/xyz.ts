4d7b4f45501331975b9fe4abfcec4a19d095b543
liuxspro <liuxspro@gmail.com>
2026-02-04 23:59:11 +0800

    Chore: 增加CRS84XYZ

--- packages/libs/src/geo/xyz.ts
+++ packages/libs/src/geo/xyz.ts
@@ -3,15 +3,34 @@ import { d2r, r2d } from "./utils.ts";
 import { xyz_to_quad as to_bing_quad } from "./quad/BingQuad.ts";
 import { xyz_to_quad as to_ge_quad } from "./quad/googleEarthQuad.ts";
 
-export class XYZ {
+class BaseXYZ {
   /**
-   * 构造函数，创建一个XYZ坐标点(墨卡托投影)
+   * 构造函数，创建一个XYZ坐标点
    * @param x - X坐标（瓦片列索引）
    * @param y - Y坐标（瓦片行索引）
    * @param z - 缩放级别（zoom level）
    */
-  constructor(public x: number, public y: number, public z: number) {}
+  constructor(
+    public readonly x: number,
+    public readonly y: number,
+    public readonly z: number,
+  ) {}
 
+  /**
+   * 从 XYZ 坐标生成对应的 XYZ 瓦片坐标对象
+   * @param x - X坐标（瓦片列索引）
+   * @param y - Y坐标（瓦片行索引）
+   * @param z - 缩放级别（zoom level）
+   * @returns 对应的XYZ瓦片坐标对象
+   * @remarks
+   * 该方法仅作为构造函数的别名，便于语义化调用。
+   */
+  static from_xyz(x: number, y: number, z: number): BaseXYZ {
+    return new BaseXYZ(x, y, z);
+  }
+}
+
+export class XYZ extends BaseXYZ {
   /**
    * 将XYZ瓦片坐标转换为地理经纬度坐标
    * @returns 返回包含经度和纬度的二元组 [longitude, latitude]（单位：度）
@@ -35,14 +54,6 @@ export class XYZ {
   }
 
   /**
-   * 将 XYZ 瓦片坐标转换为 Google Earth Quadkey
-   * @returns {string} Google Earth Quadkey
-   */
-  to_ge_quadkey(): string {
-    return to_ge_quad(this.x, this.y, this.z);
-  }
-
-  /**
    * 从地理经纬度坐标生成对应的XYZ瓦片坐标
    * @param lon - 经度（单位：度）
    * @param lat - 纬度（单位：度）
@@ -62,16 +73,59 @@ export class XYZ {
     );
     return new XYZ(x, y, z);
   }
-  /**
-   * 从XYZ坐标生成对应的XYZ瓦片坐标对象
-   * @param x - X坐标（瓦片列索引）
-   * @param y - Y坐标（瓦片行索引）
-   * @param z - 缩放级别（zoom level）
-   * @returns 对应的XYZ瓦片坐标对象
-   * @remarks
-   * 该方法仅作为构造函数的别名，便于语义化调用。
-   */
-  static from_xyz(x: number, y: number, z: number): XYZ {
+
+  static override from_xyz(x: number, y: number, z: number): XYZ {
     return new XYZ(x, y, z);
+  }
+}
+
+export class CRS84XYZ extends BaseXYZ {
+  /**
+   * 将CRS84的XYZ瓦片坐标转换为地理经纬度坐标(瓦片中心)
+   * @returns 返回瓦片中心点坐标 [lon,lat]
+   */
+  to_lonlat(): Point {
+    const lon = (this.x * 360) / Math.pow(2, this.z) - 180;
+    // 计算瓦片中心点坐标
+    const d_lon = 360 / Math.pow(2, this.z);
+    const lat = 90 - (this.y * 180) / Math.pow(2, this.z - 1);
+    const d_lat = 180 / Math.pow(2, this.z - 1);
+    return [lon + d_lon / 2, lat - d_lat / 2];
+  }
+
+  /**
+   * 将 XYZ 瓦片坐标转换为 Google Earth Quadkey
+   * @returns {string} Google Earth Quadkey
+   */
+  to_ge_quadkey(): string {
+    return to_ge_quad(this.x, this.y, this.z);
+  }
+
+  /**
+   * 从经纬度坐标生成对应的 CRS84 瓦片坐标
+   * @param lon - 经度（单位：度）
+   * @param lat - 纬度（单位：度）
+   * @param z - 目标缩放级别
+   * @returns 对应缩放级别的 CRS84 瓦片坐标对象
+   */
+  static from_lonlat(lon: number, lat: number, z: number): CRS84XYZ {
+    // 计算瓦片总数
+    const tilesCount = Math.pow(2, z);
+
+    // 计算经度对应的x坐标
+    // 经度范围从-180到180，转换为0到tilesCount
+    const normalizedLon = (lon + 180) / 360;
+    const x = Math.floor(normalizedLon * tilesCount);
+
+    // 计算纬度对应的y坐标
+    // 纬度范围从-90到90，转换为0到tilesCount/2
+    const normalizedLat = (90 - lat) / 180;
+    const y = Math.floor(normalizedLat * (tilesCount / 2));
+
+    return new CRS84XYZ(x, y, z);
+  }
+
+  static override from_xyz(x: number, y: number, z: number): CRS84XYZ {
+    return new CRS84XYZ(x, y, z);
   }
 }
--- packages/libs/tests/geo/xyz_test.ts
+++ packages/libs/tests/geo/xyz_test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "jsr:@std/assert";
-import { XYZ } from "../../src/geo/xyz.ts";
+import { CRS84XYZ, XYZ } from "../../src/geo/xyz.ts";
 
 Deno.test("Geo: Class XYZ", () => {
   const x = 211;
@@ -10,11 +10,25 @@ Deno.test("Geo: Class XYZ", () => {
   assertEquals(xyz.to_lonlat(), lonlat);
   assertEquals(XYZ.from_lonlat(...lonlat, z), xyz);
   assertEquals(xyz.to_bing_quadkey(), "13210211");
-  assertEquals(xyz.to_ge_quadkey(), "012023022");
+
   assertEquals(new XYZ(2, 2, 2).to_bing_quadkey(), "30");
 
   const xyz2 = XYZ.from_xyz(469, 171, 9);
   const lonlat2: [number, number] = [150.170857, 50.949096];
   assertEquals(xyz2.to_lonlat(), [149.765625, 51.17934297928927]);
   assertEquals(XYZ.from_lonlat(...lonlat2, 9), xyz2);
+});
+
+Deno.test("Geo: Class CRS84XYZ", () => {
+  const x = 104;
+  const y = 20;
+  const z = 7;
+  const xyz = new CRS84XYZ(x, y, z);
+  const lonlat = [113.90625, 32.34375];
+  assertEquals(xyz.to_lonlat(), lonlat);
+  assertEquals(xyz.to_ge_quadkey(), "02102033");
+  assertEquals(
+    CRS84XYZ.from_lonlat(114.686055, 31.707519, 10),
+    new CRS84XYZ(838, 165, 10),
+  );
 });
