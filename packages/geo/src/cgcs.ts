8f8c8c566675371f1f268a1d6ef166e5c66ddf91
liuxspro <liuxspro@gmail.com>
2026-09-07 22:30:00 +0800

    Correct projection coordinate order in cgcs

--- packages/geo/src/cgcs.ts
+++ packages/geo/src/cgcs.ts
@@ -1,3 +1,6 @@
+import type { Point } from "@liuxspro/libs/geo";
+import { get_digits } from "@liuxspro/libs/utils";
+
 /**
  * 返回带号对应的 ESRI WKT
  * @param zone 3度带带号，25~45
@@ -31,4 +34,33 @@ export function get_zone(longitude: number): number {
     throw new Error("经度不在中国范围内(73.62~135)");
   }
   return Math.round(longitude / 3);
+}
+
+/**
+ * 纠正投影坐标顺序
+ * proj4 坐标顺序是[东坐标(加带号8位数) , 北坐标(7位)]
+ *
+ * 约定X为东坐标（横坐标，需加带号）
+ *
+ * 约定Y为北坐标（纵坐标，为恒为正的7位数）
+ * @param {Point} point 点坐标
+ * @returns points
+ */
+export function correct_points_order(point: Point): Point {
+  const x = point[0];
+  const y = point[1];
+  let real_x;
+  let real_y;
+  if (get_digits(x) == 7) {
+    // X 为7位数，X和Y互换位置
+    real_x = y;
+    real_y = x;
+  } else {
+    real_x = x;
+    real_y = y;
+  }
+  if (get_digits(real_x) != 8) {
+    throw new Error("X 缺失带号");
+  }
+  return [real_x, real_y];
 }
