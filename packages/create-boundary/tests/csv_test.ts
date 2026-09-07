c3d63c152610f7ad829b9874220352f91638aba0
liuxspro <liuxspro@gmail.com>
2026-09-07 22:32:11 +0800

    Test CSV polygon data without zone number throws

--- packages/create-boundary/tests/csv_test.ts
+++ packages/create-boundary/tests/csv_test.ts
@@ -1,7 +1,7 @@
 import { join } from "jsr:@std/path";
 import { parse_csv_content } from "../src/source/csv.ts";
 import { get_polygon_from_csv_data } from "../src/utils.ts";
-import { assertEquals } from "jsr:@std/assert";
+import { assertEquals, assertThrows } from "jsr:@std/assert";
 
 const dirname = import.meta.dirname;
 let csv_dir: string;
@@ -15,10 +15,21 @@ Deno.test("CSV: get_polygon_from_csv_data", function () {
       const file_path = join(csv_dir, dir_entry.name);
       const text = Deno.readTextFileSync(file_path);
       const csv_data = parse_csv_content(text);
+      // 缺失带号报错
+      if (file_path.includes("无带号")) {
+        assertThrows(
+          () => get_polygon_from_csv_data(csv_data),
+          Error,
+          "X 缺失带号",
+        );
+        continue;
+      }
       const polygon = get_polygon_from_csv_data(csv_data);
-      const first_point = polygon.first_point;
-      assertEquals(typeof first_point[0], "number");
-      assertEquals(typeof first_point[1], "number");
+      const x = polygon.first_point[0];
+      const y = polygon.first_point[1];
+      assertEquals(typeof x, "number");
+      assertEquals(Math.round(x).toString().length, 8);
+      assertEquals(typeof y, "number");
     }
   }
 });
